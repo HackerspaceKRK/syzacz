e440e4b410f8f632ad70d3cfdbf36fb4bcaa1805
pidpawel <git@pidpawel.eu>
2016-10-31 01:23:40 +0100

    Refactor, schema upgrade support

--- syzacz/session.py
+++ syzacz/session.py
@@ -2,7 +2,6 @@ from pprint import pprint
 from urllib.parse import urlparse
 from ldap3 import Server, Connection, SIMPLE, SYNC, ASYNC, SUBTREE, ALL, MODIFY_ADD, MODIFY_DELETE
 from configparser import ConfigParser
-from prettytable import PrettyTable
 import os.path
 
 
@@ -17,7 +16,6 @@ class Session:
 
         url = urlparse(self.ldap_config.get('url'))
 
-        # define the server and the connection
         self.server = Server(url.hostname, port=url.port, get_info=ALL)
         self.connection = Connection(self.server, auto_bind=True, client_strategy=SYNC,
                                      user=self.ldap_config.get('bind_dn'), password=self.ldap_config.get('passwd'),
@@ -26,141 +24,144 @@ class Session:
     def close(self):
         self.connection.unbind()
 
-    def query_table(self, search_base, search_filter, fields):
-        print(search_filter)
-        t = PrettyTable(fields)
+    def get_simple(self, search_base, attributes, search_filter=None, flaten=None):
+        search_filter = search_filter or '(objectClass=*)'
 
-        for field in fields:
-            t.align[field] = "l"
-
-        entries = self.connection.extend.standard.paged_search(search_base=search_base,
-                                                               search_filter=search_filter,
-                                                               search_scope=SUBTREE,
-                                                               attributes=fields,
-                                                               paged_size=10,
-                                                               generator=True)
-
-        for entry in entries:
-            if len(entry['attributes']) <= 0:
-                continue
-
-            f = []
-
-            for field in fields:
-                if field in entry['attributes']:
-                    f.append(', '.join(entry['attributes'][field]))
-                else:
-                    f.append('---')
-
-            t.add_row(f)
-
-        return str(t)
-
-    def query_debug(self, search_base, search_filter, *args, **kwargs):
-        self.connection.search(search_base=search_base,
-                               search_filter=search_filter,
-                               search_scope=SUBTREE)
-        response = self.connection.response
-        result = self.connection.result
-        for r in response:
-            print(r['dn'], r['attributes'])  # return formatted attributes
-            # print(r['dn'], r['raw_attributes'])  # return raw (bytes) attributes
-        print(result)
-
-    def list_members(self):
-        return self.query_table(search_base=self.ldap_config.get('user_ou'),
-                                search_filter="(&(objectClass=*)(memberof=cn=members,{group_ou}))".format(group_ou=self.ldap_config.get('group_ou')),
-                                fields=["uid", "cn", "mail"])
-
-    def list_users(self):
-        search_base = self.ldap_config.get('user_ou')
-        search_filter = '(objectClass=*)'
-        fields = ["uid", "cn", "mail"]
-
-        t = PrettyTable(fields + ['groups'])
-
-        for field in fields:
-            t.align[field] = 'l'
-        t.align['groups'] = 'l'
+        if flaten is False:
+            flaten = []
+        else:
+            flaten = flaten or ['cn', 'uid']
 
         entries = self.connection.extend.standard.paged_search(search_base=search_base,
                                                                search_filter=search_filter,
                                                                search_scope=SUBTREE,
-                                                               attributes=fields,
+                                                               attributes=attributes,
                                                                paged_size=10,
                                                                generator=True)
 
+        results = []
+
         for entry in entries:
-            if len(entry['attributes']) <= 0:
+            if len(entry['attributes']) == 0:
                 continue
 
-            f = []
+            result = entry['attributes']
+            result['dn'] = entry['dn']
 
-            for field in fields:
-                if field in entry['attributes']:
-                    f.append(', '.join(entry['attributes'][field]))
-                else:
-                    f.append('---')
+            for attribute_name in result:
+                if attribute_name in flaten:
+                    result[attribute_name] = result[attribute_name][0]
 
-            self.connection.search(search_base=self.ldap_config.get('group_ou'),
-                                   search_filter='(&(objectClass=*)(member={dn}))'.format(dn=entry['dn']),
-                                   search_scope=SUBTREE,
-                                   attributes=['cn'])
+            results.append(result)
 
-            g_response = self.connection.response
-            groups = [', '.join(gentry['attributes']['cn']) for gentry in g_response]
-            f.append(', '.join(groups))
+        return results
 
-            t.add_row(f)
+    def get_groups(self, attributes=None):
+        attributes = attributes or ['cn', 'gidNumber', 'description']
+        return self.get_simple(search_base=self.ldap_config.get('group_ou'),
+                               attributes=attributes)
 
-        return str(t)
+    def get_users(self, attributes=None):
+        attributes = attributes or ['uid', 'cn', 'mail']
+
+        casual = list(attributes)
+        casual.remove('groups')
+
+        users = self.get_simple(search_base=self.ldap_config.get('user_ou'),
+                                attributes=casual)
+
+        if 'groups' in attributes:
+            for user in users:
+                user['groups'] = [group['cn'] for group in self.get_user_groups(uid=user['uid'])]
+
+        return users
+
+    def get_user_dn(self, uid):
+        return 'uid={uid},{user_ou}'.format(user_ou=self.ldap_config.get('user_ou'), uid=uid)
+
+    def get_group_dn(self, cn):
+        return 'cn={cn},{group_ou}'.format(group_ou=self.ldap_config.get('group_ou'), cn=cn)
+
+    def get_user_groups(self, uid, attributes=None):
+        user_dn = self.get_user_dn(uid)
+
+        groups = self.get_simple(
+            search_base=self.ldap_config.get('group_ou'),
+            search_filter='(|(member={dn})(memberUid={uid}))'.format(dn=user_dn, uid=uid),
+            attributes=attributes or ['cn'])
+
+        return groups
 
     def add_user(self, username, common_name, email=None):
         attributes = {'cn': common_name,
                       'sn': common_name.split()[-1],
                       'mail': email}
-        self.connection.add(dn="uid={username},{user_ou}".format(username=username, user_ou=self.ldap_config.get('user_ou')),
-                            object_class=['inetOrgPerson', 'organizationalPerson', 'person', 'top'],
-                            attributes=attributes)
 
-        print(self.connection.result)
+        self.connection.add(
+            dn="uid={username},{user_ou}".format(username=username, user_ou=self.ldap_config.get('user_ou')),
+            object_class=['inetOrgPerson', 'organizationalPerson', 'person', 'top'],
+            attributes=attributes)
 
-        return self.connection.result['description'] == 'success'
+        self.upgrade_user_schema(username=username)
+
+    def get_top_uid(self):
+        users = self.get_simple(search_base=self.ldap_config.get('user_ou'),
+                                attributes=['uidNumber'])
+        return max([user['uidNumber'] for user in users])
+
+    def _migration0(self, username):
+        user_dn = self.get_user_dn(uid=username)
+
+        self.connection.modify(
+            dn=user_dn,
+            changes={
+                'objectClass': [(MODIFY_ADD, ['posixAccount'])],
+                'loginShell': [(MODIFY_ADD, ['/bin/bash'])],
+                'homeDirectory': [(MODIFY_ADD, ['/home/{}'.format(username)])],
+                'gidNumber': [(MODIFY_ADD, ['1999'])],
+                'uidNumber': [(MODIFY_ADD, [self.get_top_uid() + 1])],
+            })
+
+    def upgrade_user_schema(self, username):
+        self._migration0(username=username)
+
+    def delete_user(self, username):
+        user_dn = self.get_user_dn(uid=username)
+        groups = self.get_user_groups(uid=username)
+
+        for group in groups:
+            self.delete_from_group(username=username, group=group['cn'])
+
+        self.connection.delete(user_dn)
 
     def change_password(self, username, password):
-        self.connection.extend.standard.modify_password(user="uid={username},{user_ou}".format(username=username, user_ou=self.ldap_config.get('user_ou')),
+        self.connection.extend.standard.modify_password(user=self.get_user_dn(uid=username),
                                                         new_password=password)
 
     def add_to_group(self, username, group):
-        user_dn = "uid={username},{user_ou}".format(username=username, user_ou=self.ldap_config.get('user_ou'))
-        self.connection.modify(dn="cn={group_name},{group_ou}".format(group_name=group, group_ou=self.ldap_config.get('group_ou')),
+        user_dn = self.get_user_dn(uid=username)
+        group_dn = self.get_group_dn(cn=group)
+
+        self.connection.modify(dn=group_dn,
                                changes={'member': [(MODIFY_ADD, [user_dn])]})
 
     def delete_from_group(self, username, group):
-        user_dn = "uid={username},{user_ou}".format(username=username, user_ou=self.ldap_config.get('user_ou'))
-        self.connection.modify(dn="cn={group_name},{group_ou}".format(group_name=group, group_ou=self.ldap_config.get('group_ou')),
+        user_dn = self.get_user_dn(uid=username)
+        group_dn = self.get_group_dn(cn=group)
+
+        self.connection.modify(dn=group_dn,
                                changes={'member': [(MODIFY_DELETE, [user_dn])]})
+
+    def add_to_groups(self, username, groups):
+        for group in groups:
+            self.add_to_group(username=username, group=group)
+
+    def delete_from_groups(self, username, groups):
+        for group in groups:
+            self.delete_from_group(username=username, group=group)
 
     def activate(self, username):
         self.add_to_group(username=username, group='members')
 
     def deactivate(self, username):
         self.delete_from_group(username=username, group='members')
-
-    def xmpp_activate(self, username):
-        self.add_to_group(username=username, group='xmpp')
-
-    def xmpp_deactivate(self, username):
-        self.delete_from_group(username=username, group='xmpp')
-
-    def staff_activate(self, username):
-        self.add_to_group(username=username, group='staff')
-
-    def staff_deactivate(self, username):
-        self.delete_from_group(username=username, group='staff')
-
-    def admin_activate(self, username):
-        self.add_to_group(username=username, group='admin')
-
-    def admin_deactivate(self, username):
-        self.delete_from_group(username=username, group='admin')
